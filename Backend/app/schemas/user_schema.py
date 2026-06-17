587df7a6552fa247f332497a7f1bf4d633543ae4
LukaoModesto <lucasoliveiractt@outlook.com>
2026-06-17 11:25:13 -0300

    feat: add user input validation

--- Backend/app/schemas/user_schema.py
+++ Backend/app/schemas/user_schema.py
@@ -1,14 +1,43 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 class UserCreate(BaseModel):
-    name: str
+    name: str = Field(
+        min_length=2,
+        max_length=80
+    )
     email: EmailStr
-    password: str
+    password: str = Field(
+        min_length=6,
+        max_length=72
+    )
 
+    @field_validator("name")
+    @classmethod
+    def validate(cls, value):
+        value = value.strip()
+
+        if not value:
+            raise ValueError("O nome nao pode estar vazio.")
+        
+        return value
+    
+    @field_validator("email")
+    @classmethod
+    def normalize_email(cls, value):
+        return value.lower()
+    
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        if value.strip() != value:
+            raise ValueError("A senha não pode começar ou terminar com espaços.")
+        
+        return value
+    
 class UserResponse(BaseModel):
-    id: int
-    name: str
-    email: EmailStr
+    id:int
+    name:str
+    email:EmailStr
 
     class Config:
         from_attributes = True
